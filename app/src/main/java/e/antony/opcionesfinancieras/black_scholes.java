8b5aa60585cc13ca99ecad6c3412c156dee8e17a
KillerDiscret <carlos_p_s_1998@hotmail.com>
2018-11-15 02:12:52 -0500

    [cambio2]

---
+++ app/src/main/java/e/antony/opcionesfinancieras/black_scholes.java
@@ -0,0 +1,13 @@
+package e.antony.opcionesfinancieras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class black_scholes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_black_scholes);
+    }
+}
